a1b9bb203bc062b1cd33727f4340b16292885256
py <123456@qq.com>
2019-04-01 14:31:48 +0800

    learn email

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -20,9 +20,6 @@ public class Application extends Controller {
     public static void check(){
         if (session.get("user")!=null){
             render("Application/index.html");
-            System.out.println("change by bravery");
-            System.out.println("change by bravery");
-            System.out.println("brave change something");
         }
     }
 
--- app/controllers/UserController.java
+++ app/controllers/UserController.java
@@ -1,9 +1,13 @@
 package controllers;
 
 import models.User;
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
 import play.data.validation.Required;
+import play.libs.Mail;
 import play.mvc.Controller;
 import play.cache.*;
+import play.libs.mail.*;
 
 public class UserController extends Controller {
     //跳转到登录页面
@@ -30,5 +34,14 @@ public class UserController extends Controller {
     public static void logout(){
         session.clear();
         render("Application/login.html");
+    }
+    public static void sendMail() throws EmailException {
+        SimpleEmail e = new SimpleEmail();
+        e.setFrom("2472937751@qq.com");
+        e.addTo("1075656423@qq.com");
+        e.setSubject("subject");
+        e.setMsg("Message");
+        Mail.send(e);
+        render("Application/success.html");
     }
 }
